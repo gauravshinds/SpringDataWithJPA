1d57b287ba6d636e15264057f7b0d09af6819afb
gauravshinds <shindegs.gaurav@gmail.com>
2025-02-13 19:56:56 +0530

    Add files via upload

---
+++ src/main/java/com/spring/data/jpa/tutorial/entity/Guardian.java
@@ -0,0 +1,39 @@
+package com.spring.data.jpa.tutorial.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.AttributeOverride;
+import javax.persistence.AttributeOverrides;
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+
+@Embeddable
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@AttributeOverrides({
+        @AttributeOverride(
+                name = "name",
+                column = @Column(name = "guardian_name")
+        ),
+        @AttributeOverride(
+                name = "email",
+                column = @Column(name = "guardian_email")
+        ),
+        @AttributeOverride(
+                name = "mobile",
+                column = @Column(name = "guardian_mobile")
+        )
+})
+public class Guardian {
+
+    private String name;
+    private String email;
+    private String mobile;
+}
